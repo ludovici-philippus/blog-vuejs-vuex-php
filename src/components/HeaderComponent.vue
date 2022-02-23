1548aa314055a70f41cdf4c4cd0c0316e8256f33
Luis Felipe <202makertv@gmail.com>
2022-02-23 12:25:47 -0300

    Configurações iniciais

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div class="overlay"></div>
+        <div class="logo">
+            <router-link to="/"><h1>Blog</h1></router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name: "HeaderComponent"
+    }
+</script>
+
+<style scoped>
+header{
+
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    busca: ""
   },
   getters: {
+    getBusca: state => {
+        return state.busca;
+    }
   },
   mutations: {
+      buscar(state, texto){
+          state.busca = texto;
+      }
   },
   actions: {
   },
